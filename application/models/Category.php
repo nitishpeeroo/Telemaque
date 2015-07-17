5bed332e721b75e394eb07145ccc99ef32a07f3c
nitishpeeroo <nitish@alphamedia.fr>
2015-07-17 10:46:55 +0200

    panier  et category

--- application/models/Category.php
+++ application/models/Category.php
@@ -7,11 +7,11 @@ class Application_Model_Category extends Zend_Db_Table_Abstract {
     public $data = array();
 
     public function getRubrique() {
-
         $select = $this->select()
                 ->setIntegrityCheck(false)
-                ->from(array('a' => DB_TABLE_CATEGORY), array('a.id_category','a.label_category', 'a.parent'));
-               // ->joinInner(array('i' => DB_TABLE_IMAGE_WEBSITE),'a.id_category = i.id_category');
+                ->from(array('c' => DB_TABLE_CATEGORY), array('c.id_category', 'c.label_category', 'c.parent'))
+                ->where('c.parent = 0 ');
+        // ->joinInner(array('i' => DB_TABLE_IMAGE_WEBSITE),'a.id_category = i.id_category');
         try {
             $tab = $this->fetchAll($select)->toArray();
         } catch (Exception $ex) {
@@ -19,7 +19,49 @@ class Application_Model_Category extends Zend_Db_Table_Abstract {
             echo 'ERROR_SELECT_GETARTICLES : ' . $ex->getMessage();
             return false;
         }
+        $this->data = $tab;
+        return $this->data;
+    }
 
+    public function getSousRubrique() {
+        $select = $this->select()
+                ->setIntegrityCheck(false)
+                ->from(array('c' => DB_TABLE_CATEGORY), array('c.id_category', 'c.label_category', 'c.parent'))
+                ->joinInner(array('i' => DB_TABLE_IMAGE_WEBSITE), 'c.id_category = i.id_category')
+                ->where('c.parent !=  0 ');
+        try {
+            $tab = $this->fetchAll($select)->toArray();
+        } catch (Exception $ex) {
+
+            echo 'ERROR_SELECT_GETARTICLES : ' . $ex->getMessage();
+            return false;
+        }
+        $this->data = $tab;
+        return $this->data;
+    }
+
+    public function convertImageSousRubrique($ssrubrique) {
+        foreach ($ssrubrique as &$p) {
+            $p['img64'] = base64_encode($p['image']);
+            $p['type'] = pathinfo($p['name_image'], PATHINFO_EXTENSION);
+        }
+        return $ssrubrique;
+    }
+
+    public function getArticleByCategory($categorie) {
+        $select = $this->select()
+                ->setIntegrityCheck(false)
+                ->from(array('s' => DB_TABLE_SELL), array('s.id_sell', 's.id_user', 's.id_category', 's.description', 's.price', 's.quantity', 's.is_checked', 's.title', 's.dt_creation', 's.dt_modification', 's.description_courte'))
+                ->joinInner(array('c' => DB_TABLE_CATEGORY), 'c.id_category = s.id_category')
+                ->joinInner(array('i' => DB_TABLE_IMAGE), 's.id_sell = i.id_sell')
+                ->where('c.id_category =?', $categorie);
+        try {
+            $tab = $this->fetchAll($select)->toArray();
+        } catch (Exception $ex) {
+
+            echo 'ERROR_SELECT_GETARTICLES : ' . $ex->getMessage();
+            return false;
+        }
         $this->data = $tab;
         return $this->data;
     }
